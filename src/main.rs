674d2c8425235552cd5dbe12043419538c08e2d6
kingslayer254 <markpollycarp@gmail.com>
2022-05-27 11:07:23 +0300

    e

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i32 = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    const SUBSCRIBER_COUNT: u32 = 100000; 
 }
